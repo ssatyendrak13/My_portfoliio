bbb99f36df85510d0b54a4e09a1d9e8e69115ac0
Satyendra Kumar <98349239+ssatyendrak13@users.noreply.github.com>
2023-10-29 01:39:48 +0530

    first Commit

---
+++ src/data/constants.js
@@ -0,0 +1,366 @@
+export const Bio = {
+  name: "Satyendra Kumar",
+  roles: [
+    "Full Stack Java Developer",
+    "Android Developer",
+    "Programmer",
+  ],
+  description:
+    "Analyst(Software Engineer) at Capgemini, Ability to perform well in a team. Developing my skills as a full stack developer. I am a team player with Excellent problem solving skill, passionate about coding. Love to do competitive programming on daily basis.",
+  github: "https://github.com/ssatyendrak13",
+  resume:
+    "https://docs.google.com/document/d/1JHroRCSTwDUJ0L0NZqquYvMd_o6KIiFqPiSTa1gEvIs/edit",
+  linkedin: "https://www.linkedin.com/in/sstyendra13/",
+  twitter: "https://twitter.com/SatyendraUrs",
+  insta: "https://www.instagram.com/ssatyendra13/",
+  facebook: "https://www.facebook.com/satyendra.sinha.566?mibextid=ZbWKwL",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+     
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+     
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://successinsightsindia.com/wp-content/uploads/2022/02/Coding-Ninjas-Logo.jpg",
+    role: "Campus Ambassador",
+    company: "Coding Ninjas",
+    date: "Jan 2022 - June 2022",
+    desc: "Coding Ninjas is a online learning platform. My role was there as a campus ambassador to do advertisement of coding ninjas courses",
+    skills: [
+      "English Communication",
+      "Linkedin",
+      "whatsapp",
+      "instagram",
+      "discord",
+     
+    ],
+    // doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+
+    id: 0,
+    img: "https://tse1.mm.bing.net/th?id=OIP.G_KUElpogPsJINRaprtZxgHaB6&pid=Api&P=0&h=180",
+    role: "Full Stack Java Developer Intern",
+    company: "Capgemini Technology Pvt Ltd",
+    date: "Jan 2023 - April 2023",
+    desc: "I was full stack java developer learnt many technology and on the basis of that particular technology we designed the project that was Online shopping cart system with team member.",
+    skills: [
+      "Java",
+      "SpringBoot",
+      "Maven",
+      "JUnit",
+      "JPA",
+      "Rest API",
+      "Spring Security",
+      "Postgres",
+      "html",
+      "css",
+      "JavaScript",
+      "Rect js",
+      "Node Js",
+      "TypeScript",
+    ],
+   
+    // doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  {
+    id: 0,
+    img: "https://tse1.mm.bing.net/th?id=OIP.G_KUElpogPsJINRaprtZxgHaB6&pid=Api&P=0&h=180",
+    role: "Analyst",
+    company: "Capgemini Technology Pvt Ltd",
+    date: "26 oct 2023 - Present",
+    desc: "I started my career from capgemini technology as an Analyst. My domain is in this company is full stack java which include the technology like java, spring boot, javaScript, react and etc.",
+    skills: [
+      "Java",
+      "SpringBoot",
+      "Maven",
+      "JUnit",
+      "JPA",
+      "Rest API",
+      "Spring Security",
+      "Postgres",
+      "html",
+      "css",
+      "JavaScript",
+      "Rect js",
+      "Node Js",
+      "TypeScript",
+    ],
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://collegekhabri.com/data/technocrats-institute-of-technology-logo.jpg",
+    school: "Technocrats Institute of Technology Bhopal Madhya Pradesh",
+    date: "Aug 2019 - May 2023",
+    grade: "9.10 CGPA",
+    desc: "I have Completed bachelor of Technology from Technocrats Institute of Technology in Electronics and Communication Engineering Branch. Ready to join Capgemini as an Analyst  and Learning Multiple technology to become master in full stack development. I am a team player with Excellent problem solving skill, passionate about.",
+    degree: "Bachelor of Technology - BTech, Electronics and Communication Engineering",
+  },
+  {
+    id: 1,
+    img: "https://www.jcbakewar.org.in/images/26.jpg",
+    school: "Janta College Laukaha Madhubani Bihar ",
+    date: "June 2016 - April 2018",
+    grade: "66.20%",
+    desc: "I completed my class 12 from Janta College Laukaha Madhubani Bihar. Taken Physics, Chemistry, Mathematics, English and Hindi.",
+    degree: "ISC(XII), Science",
+  },
+  {
+    id: 2,
+    img: "https://tse3.mm.bing.net/th?id=OIP.5tsdp__MNv-0-QYTvgpWdwAAAA&pid=Api&P=0&h=180",
+    school: "+2 Ishwari Bhagat High School Goth Parsahi Madhubani Bihar",
+    date: "March 2014 - Apr 2016",
+    grade: "61.2%",
+    desc: "I completed my class 10 education from +2 Ishwari Bhagat High School goth Parsahi Madhubani Bihar.",
+    degree: "BSEB(X), Common Subject",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Shuffler",
+    date: "Sep 2022 - oct 2022",
+    description:
+      "This simple music streaming app which is built in android the purpose of the Shuffler is to play music available in the databases of the android phone and compose custom playlists to differentiate the songs with the similar need.",
+    image:
+      "https://cdn.dribbble.com/users/1012608/screenshots/14182150/media/c8e4531aac510bc7fe31e9e8fe507228.png",
+    tags: [
+      "Java",
+      "Kotlin",
+      "Android",
+      "Sqlite",
+      "Google play console",
+      
+    ],
+    category: "Android",
+    github: "https://github.com/ssatyendrak13/Shuffler",
+    webapp: "https://play.google.com/store/apps/details?id=com.myDomain.suffler",
+  },
+  {
+    id: 0,
+    title: "Cafe Billing system",
+    date: "Jan 2022 - Feb 2022",
+    description:
+      "This is freelance project which is made for the restaurant manager to manage all the requirement. This application saves the buyer details as well as product details also can update the details, delete the buyer and product details. This app is basically made for saving the time of café manager to manage the café in well manner.",
+    image:
+      "https://tse3.mm.bing.net/th?id=OIP.e5p_Qsd0HK0uYAZ0b-f5CAHaEc&pid=Api&P=0&h=180",
+    tags: ["Basic java", "Oops", "Swings", "AWT", "JDBC" , "MySql"],
+    category: "Java",
+    github: "https://github.com/ssatyendrak13/Cafe-Billing",
+    webapp: "https://drive.google.com/file/d/1VS0puxsj41cjUgQPQmtgACm4ZobwlHjK/view",
+    
+  },
+  {
+    id: 1,
+    title: "TrueValue",
+    date: "April 2022 - May 2022",
+    description:
+      "An True Value project is a desktop application developed for users to vehicle resale.  This system overcomes the problem of searching for a second hand vehicle.  People usually go to garages and showrooms in search for their desired second hand vehicle  that they wish to buy that becomes tedious at a point and even consumes lot of time. Here our application help garage owner to manage car,buyer sellers record Hence the project helps users to get their desired vehicle details  at a single place. The project is categorized into various categories for each type of vehicle like bus, car, truck, scooters  and so on where sellers can register their second hand vehicle ads for sale in respective category.  Users can view and browse through these categories and request for their desired vehicle by selecting  the vehicle. The request is sent to buyer Ans seller   and car detail In future the system provides an additional feature of vehicle insurance for users to avail it. .",
+    image:
+      "https://thumbs.dreamstime.com/b/true-value-rubber-stamp-grunge-design-dust-scratches-effects-can-be-easily-removed-clean-crisp-look-color-easily-97604640.jpg",
+      tags: ["Basic java", "Oops", "Swings", "AWT", "JDBC" , "MySql"],
+    category: "web app",
+    github: "https://github.com/ssatyendrak13/TrueValue-Project",
+    webapp: "https://drive.google.com/file/d/1Ezv5uYxwif0TUVWLjp-Katu7GLPPezW_/view",
+  },
+  {
+    id: 2,
+    title: "Quiz Application",
+    date: "Dec 2021 - Dec 2021",
+    description:
+      "simple java quiz application in which every quiz has own specific time and all quiz will display one after another . technology use in this application are awt and swings.",
+    image:
+      "https://cdn.wallpapersafari.com/74/13/z2BZAw.jpg",
+    tags: ["Basic Java", "Oops", "swings", "awt"],
+    category: "Java",
+    github: "https://github.com/ssatyendrak13/quiz-application",
+    // webapp: "https://brain-tumor.netlify.app/",
+    // member: [
+    //   {
+    //     name: "Rishav Chanda",
+    //     img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+    //     linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+    //     github: "https://github.com/rishavchanda/",
+    //   },
+    //   {
+    //     name: "Upasana Chaudhuri",
+    //     img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+    //     linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+    //     github: "https://github.com/upasana0710",
+    //   },
+    // ],
+  },
+  {
+    id: 3,
+    title: "Chatting Application",
+    date: " March 2022 - March 2022",
+    description:
+      "this is simple desktop application where client and server pass the message to each other . By typing the exit message client and server can terminate the chat and get the message dialog either on client area or server area",
+    image:
+      "https://cdn.dribbble.com/users/3745465/screenshots/14066363/media/f6897a21464f69a586e73f2fb4cb6683.png",
+    tags: ["Basic Java", "Oops", "Swings", "Awt" , "Java Networking"],
+    category: "Java",
+    github: "https://github.com/ssatyendrak13/chat-application",
+    // webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
+  },
+  {
+    id: 10,
+    title: "Snake Gamming App",
+    date: "June 2022 - June 2020",
+    description:
+      "Snake can move in a given direction and when it eats the food, the length of snake increases. • When snake crosses itself, the game will over. Food can appear on random position of JFrame.",
+    image:
+      "https://freeappsforme.com/wp-content/uploads/2020/10/Snakes-and-Ladders-3D-Multiplayer1-2048x2048.jpg",
+    tags: ["Basic Java","Oops","swing"],
+    category: "Java",
+    github: "https://github.com/ssatyendrak13/snake-game",
+    // webapp: "https://github.com/rishavchanda/Job-finder-App",
+  },
+  {
+    id: 4,
+    title: "Online Shopping Cart System",
+    date: "Jan 2023 - April 2023",
+    description:
+      "E-Commerce websites work through a series of steps, utilizing website code, the database, and 3rd party applications such as a payment processor or payment gateway. The Basic technology used in this ecommerce website is java, springBoot, REST Api, HTML, CSS, javascript, node.js, react.js and we used postgres as database.",
+    image:
+      "https://i1.wp.com/technofall.com/wp-content/uploads/2017/04/Online-shopping-cart.png?resize=765%2C514",
+    tags: ["Core Java", "SpringBoot", "MVC", "Spring Security" , "Postgres" , "JPA" , "Rest API" , "html" , "css" , "JavaScript",
+  "TypeScript","Node.js" , "React"],
+    category: "web app",
+    github: "https://github.com/ssatyendrak13/Case_Study_Capgemini",
+    // webapp: "https://whatsapp-clone-rishav.web.app",
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
